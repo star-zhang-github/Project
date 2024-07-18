a09c60d7b08af5494e9afe6dd6f7da0613bd31b0
star-zhang-github <starlichenz@gmail.com>
2024-07-18 01:57:08 -0400

    final project

--- ai_voice_chatbot/archives/get_transcript.py
+++ ai_voice_chatbot/archives/get_transcript.py
@@ -1,13 +1,13 @@
 import requests
 import os
 # from requests.auth import HTTPBasicAuth
-#from dotenv import load_dotenv
+from dotenv import load_dotenv
 # from twilio.rest import Client
 import certifi
 
-#load_dotenv()
-# account_sid = os.getenv("TWILIO_ACCOUNT_SID")
-# auth_token = os.getenv("TWILIO_AUTH_TOKEN")
+load_dotenv()
+account_sid = os.getenv("TWILIO_ACCOUNT_SID")
+auth_token = os.getenv("TWILIO_AUTH_TOKEN")
 
 
 
@@ -18,19 +18,19 @@ def user_response_transcript(account_sid, auth_token):
 
     if response.status_code == 200:
         transcription_json = response.json()
-    print(f"status code: {response.status_code}")
+        print(f"response.status_code 200")
 
     transcription = transcription_json['transcriptions'][0]
     user_response = transcription['transcription_text']
+    print(f"transcript processed")
     return user_response
 
 
 #print(user_response_transcript(account_sid, auth_token))
 
 
-# if __name__ == "__main__":
-#     print(f"status code: {response.status_code}")
-#     print(transcription_json)
+if __name__ == "__main__":
+    print(user_response_transcript(account_sid, auth_token))
 
 # def message():
 #     client = Client(account_sid, auth_token)
--- ai_voice_chatbot/building_blocks/llm.py
+++ ai_voice_chatbot/building_blocks/llm.py
@@ -17,91 +17,53 @@ class CustomAgent():
         self.question_prompt = question_prompt
         self.questions = self._load_questions()
         self.conversations = {} # dictionary mapping questions to responsees
-        self.question_idx = 0
+        #self.question_idx = 0
 
     def _load_questions(self):
-        with open('./building_blocks/questions.json', 'r') as file:
+        with open('./building_blocks/data.json', 'r') as file:
             questions = json.load(file)["questions"]
         return questions
 
-    def _ask_question(self, question):
-        prompt = self.question_prompt.format(question=question)
-        # TTS to ask the question
-        response = input(f"{prompt}: ")
-        # response = VoiceResponse()
-        # response.say(prompt)
-
-        # take the voice input from twilio and transform it back to text
-        # response = (the text transformed)
-        #print(response)
-        return response
-
-    
-    # def run_agent(self):
-
-    #     for question in self.questions:
-    #         valid_response = False
-    #         while not valid_response:
-    #             response = self._ask_question(question=question)
-    
-    #             valid_response = self.validate_response(question, response)
-    #             if valid_response:
-    #                 self.conversations[question] = response
-    #             else:
-    #                 print("I didn't quite get that. Please say again.")
-        
-    #     return self.conversations
-
-    def get_question(self):
+    def get_question(self, question_idx):
         # no more questions
-        if self.question_idx == len(self.questions):
+        if question_idx < len(self.questions):
+            question = self.questions[question_idx]
+        else:
             return -1
-        question = self.questions[self.question_idx]
-        self.question_idx += 1
         return question
-    
+
     # define value response function
-    def validate_response(self, question, response):
-        validation_prompt = f"Question: {question}\nResponse: {response}\nIs this response acceptable and reasonable? Answer with 'yes' or 'no' and explain why."
-        validation_result = client.chat.completions.create(
+    def get_response(self, question, response):
+        prompt = f"Extract the key word answer from the text provided for the question.\n Question: {question}\n Text: {response}\n. "
+        result = client.chat.completions.create(
             model="gpt-3.5-turbo",
             messages=[
-            {"role": "system", "content": "you are a call agent who can validate if the response given to the questions are valid and response with 'yes' or 'no'"},
-            {"role": "user", "content": validation_prompt}
+            {"role": "system", "content": "you are a call agent who extract the key word answer from the text for the question provided"},
+            {"role": "user", "content": prompt}
             ]
         )
-        result_text = validation_result.choices[0].message.content
+        result_text = result.choices[0].message.content
         print(f"result_text: {result_text}")
-        return "yes" in result_text.lower()
+        return result_text
 
+    def add_record(self):
+        file_path = './building_blocks/data.json'
+        with open(file_path, 'r') as file:
+            data = json.load(file)
+    
+        data["records"].append(self.conversations)
 
-# define the prompt templates for asking questions
-question_prompt = ChatPromptTemplate.from_messages([
-    ("system", "You are a chatbot acting as a call center to receive patient calls for doctor appointment"),
-    ("ai", "{question}")
-    ])
-
-# # load the list of questions from json file
-# with open('./building_blocks/questions.json', 'r') as file:
-#     questions = json.load(file)["questions"]
-
-
-# # define value response function
-# def validate_response(question, response):
-#     validation_prompt = f"Question: {question}\nResponse: {response}\nIs this response acceptable and reasonable? Answer with 'yes' or 'no' and explain why."
-#     validation_result = client.chat.completions.create(
-#         model="gpt-3.5-turbo",
-#         messages=[
-#         {"role": "system", "content": "you are a call agent who can validate if the response given to the questions are valid and response with 'yes' or 'no'"},
-#         {"role": "user", "content": validation_prompt}
-#         ]
-#     )
-#     result_text = validation_result.choices[0].message.content
-#     print(f"result_text: {result_text}")
-#     return "yes" in result_text.lower()
-
+        with open(file_path, 'w') as json_file:
+            json.dump(data, json_file, indent=4)
+        
 
 if __name__ == "__main__":
+
+    #define the prompt templates for asking questions
+    question_prompt = ChatPromptTemplate.from_messages([
+        ("system", "You are a chatbot acting as a call center to receive patient calls for doctor appointment"),
+        ("ai", "{question}")
+        ])
     custom_agent = CustomAgent(question_prompt=question_prompt)
     conversations = custom_agent.run_agent()
     for question, response in conversations.items():
